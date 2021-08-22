6ad0bcc37477e75d373398c5f0bfcb621ea04f7e
DavidNNussbaum <brocknynj@aol.com>
2021-08-22 09:42:32 -0400

    Signup.js is modified.

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,7 +4,7 @@ function Signup(props) {
     const [showForm, setShowForm] = useState(true)
     return (
     <div>
-       <h1>Account Creation</h1>
+       <h1>Create An Account</h1>
     { showForm && <SignupForm setSignupUser={props.setSignupUser} setShowForm={setShowForm}/> }
     { props.currentUser.user && <h2>{`${props.currentUser.user.user.data.attributes.first_name}, you have successfully signed up. Press on the Navigation Bar Medications and Complications tab to continue.`}</h2> } 
     </div>
